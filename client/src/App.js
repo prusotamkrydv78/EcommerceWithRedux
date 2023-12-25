4409016f195248b176b5e733308122a7fe76c651
Prusotam Yadav <prusotamyadav110@gmail.com>
2023-12-25 09:47:52 +0545

    video 6 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
- 
-import './App.css';
-
+import "./App.css";
+import {Container, NavLink } from 'react-bootstrap'
 function App() {
   return (
-    <div className="App">
-      <h1>welcome to react project</h1>
-    
-    </div>
+    <Container>
+      <div className="container">
+        <NavLink>lo</NavLink>
+        <h1>welcome to react project</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis
+          repellat, veritatis est id enim rerum ratione voluptatem provident
+          voluptatum, accusantium molestiae nemo autem cum cumque eligendi
+          incidunt placeat a iusto?
+        </p>
+      </div>
+    </Container>
   );
 }
 
