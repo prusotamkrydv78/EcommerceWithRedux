b29ab51cdb7c1e9e0f13e3fa7116790f6436a865
Prusotam Yadav <prusotamyadav110@gmail.com>
2023-12-25 16:07:27 +0545

    video 13 completed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const products = require("./data/products");
+const dotenv = require("dotenv");
 const app = express();
 const cors = require("cors");
 
@@ -10,6 +11,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+// dotenv dotenv.config
+dotenv.config();
 const PORT = 5000;
 app.get("/", (req, res) => {
   res.send("home page");
@@ -22,6 +25,6 @@ app.get("/products/:id", (req, res) => {
   const product = products.find((p) => p._id == req.params.id);
   res.json(product);
 });
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`your sever is running on ${PORT}`);
 });
