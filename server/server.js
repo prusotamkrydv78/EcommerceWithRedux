ad16fdbec8e86808a0921de622f5aa0aa759ce06
Prusotam Yadav <prusotamyadav110@gmail.com>
2023-12-25 14:39:55 +0545

    video 10 completed

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const products = require("./data/products");
+const app = express();
+
+const PORT = 5000;
+app.get("/", (req, res) => {
+    res.send('home page'); 
+});
+app.get("/products", (req, res) => {
+  //   res.send();
+  res.json(products);
+});
+app.get("/products/:id", (req, res) => {
+  const product = products.find((p) => p._id == req.params.id);
+  res.json(product)
+});
+app.listen(PORT, () => {
+  console.log(`your sever is running on ${PORT}`);
+});
