99ba27dbb7a5f1ab8c60f967fbfba399a383a01d
Prusotam Yadav <prusotamyadav110@gmail.com>
2023-12-27 18:43:33 +0545

    video 28 comp, displaying products on homescreens

--- client/src/Screens/HomeScreen.jsx
+++ client/src/Screens/HomeScreen.jsx
@@ -1,37 +1,36 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { listProduct } from "../actions/productAction";
 import { Col, Container, Row } from "react-bootstrap";
 import ProductScreen from "./ProductScreen";
-import axios from "axios";
+
+
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const { data } = await axios({
-          method: "get",
-          url: `http://localhost:5000/api/products`,
-        });
-        setProducts(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProduct());
+  }, []); 
   return (
     <>
-      <Container>
-        <Row>
-          {products.map((product) => {
-            return (
-              <Col md={3} className="my-2" key={product._id}>
-                <ProductScreen product={product} />
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+      {loading ? (
+        <h1>loading</h1>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <Container>
+          <Row>
+            {products.map((product) => {
+              return (
+                <Col md={3} className="my-2" key={product._id}>
+                  <ProductScreen product={product} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      )}
     </>
   );
 };
